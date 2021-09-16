df71524c58de6bdaff6db3f1a5e1c761833d9abc
EimantasVinciunas <eimantas.vinciunas@mif.stud.vu.lt>
2021-09-16 22:39:45 +0300

    takes input and turns it to very simple hash with no randomness

--- Hash.cpp
+++ Hash.cpp
@@ -3,6 +3,50 @@ using namespace std;
 
 int main()
 {
+    cout << "Input: \n";
+    string inp;
+    cin >> inp;
+    cout << '\n';
+    char hash[64]= {(char)0};
+    
+
+    //cout << inp.length() << endl;
+    if(inp.length() < 64)
+    {
+        for(int i =0;i<64;i++)
+        {
+            //cout << (a%inp.length()) << endl;
+            hash[i] = inp[i%inp.length()];
+        }
+    }
+    else
+    {
+        for(int i=0;i<inp.length();i++)
+        {
+            hash[i%64]^=inp[i];
+        }
+    }
+    // }
+
+    // for(int a=0;a<inp.length();a++)
+    // {
+    //     if(a<64)
+    //     {
+    //         cout << (a%inp.length()) << endl;
+    //         hash[a] = inp[a%inp.length()];
+    //     }
+    //     for(int i =0;i<64;i++)
+    //     {
+    //         //hash[i]^=inp[a];
+    //     }
+    // }
+    cout << "Hash: \n";
+    for(int i =0;i<64;i++)
+    {
+        cout << hash[i];
+    }
+    cout << endl<<endl;
+
     system("pause");
     return 0;
 }
