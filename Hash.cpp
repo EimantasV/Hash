f8f18ad65597c4d61cff08d0597efdc8570e2315
EimantasVinciunas <eimantas.vinciunas@mif.stud.vu.lt>
2021-09-20 20:46:44 +0300

    added console args.
    Hash.exe -t "textfile.txt"
    Hash.exe -m "my text"
    also -b to print as binary

--- Hash.cpp
+++ Hash.cpp
@@ -1,95 +1,219 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 using namespace std;
+string Hash(string inp, bool isBinary);
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Input: \n";
-    string inp;
-    cin >> inp;
-    cout << '\n';
-    char hash[64]= {(char)0};
-    
-
-    //cout << inp.length() << endl;
-    if(inp.length() < 64)
+    if (argc == 1)
     {
-        for(int i =0;i<64;i++)
+        cout << "Error: no args. Try (-t \"ExampleFile.txt\") or (-m \"your text\") " << endl;
+        system("pause");
+        return 0;
+    }
+
+    string FileLoc;
+    string inp;
+    bool isFromText = true;
+    bool isBinary = false;
+    for (int i = 1; i < argc; i++)
+    {
+        //cout << argv[i]<<endl;
+
+        if (argv[i][0] == '-' && argv[i][1] == 't')
         {
-            //cout << (a%inp.length()) << endl;
-            hash[i] = inp[i%inp.length()];
+            if (i + 1 < argc)
+            {
+                FileLoc = argv[i + 1];
+            }
+            else
+            {
+                cout << "Error: no input arg." << endl;
+                system("pause");
+                return 0;
+            }
         }
+        else if (argv[i][0] == '-' && argv[i][1] == 'm')
+        {
+            if (i + 1 < argc)
+            {
+                inp = argv[i + 1];
+                isFromText = false;
+            }
+            else
+            {
+                cout << "Error: no input arg." << endl;
+                system("pause");
+                return 0;
+            }
+        }
+
+        if (argv[i][0] == '-' && argv[i][1] == 'b')
+        {
+            isBinary=true;
+        }
+    }
+
+    if (isFromText)
+    {
+        ifstream GetTxt(FileLoc);
+        if (!GetTxt.is_open())
+        {
+            cout << "Error: couldn't open file." << endl;
+            system("pause");
+            return 0;
+        }
+
+        stringstream strStream;
+        strStream << GetTxt.rdbuf(); //read the file
+        string inp = strStream.str();
+
+        cout << Hash(inp,isBinary) << endl;
     }
     else
     {
-        for(int i=0;i<inp.length();i++)
-        {
-            hash[i%64]^=inp[i];
-        }
+        cout << Hash(inp,isBinary) << endl;
     }
 
-    for(int a=0;a<inp.length();a++)
+    system("pause");
+    return 0;
+}
+
+string Hash(string inp, bool isBinary)
+{
+    char hash[64] = {(char)0};
+    for (int a = 0; a < inp.length(); a++)
     {
-        for(int i =0;i<64;i++)
+        for (int i = 0; i < 64; i++)
         {
-            switch (((int)inp[a]+i)%15)
+            switch (((int)inp[a] + i + a) % 15)
             {
-                case 0:
-                    hash[i] ^= inp[i%inp.length()];
-                    break;
-                case 1:
-                    hash[i] ^= (inp[i%inp.length()]>>2);
-                    break;
-                case 2:
-                    hash[i] ^= (inp[i%inp.length()]>>1);
-                    break;
-                case 3:
-                    hash[i] ^= (inp[i%inp.length()]<<2);
-                    break;
-                case 4:
-                    hash[i] ^= (inp[i%inp.length()]<<1);
-                    break;
-                case 5:
-                    hash[i] ^= (~inp[i%inp.length()]);
-                    break;
-                case 6:
-                    hash[i] ^= (~inp[i%inp.length()]<<1);
-                    break;
-                case 7:
-                    hash[i] ^= (~inp[i%inp.length()]>>1);
-                    break;
-                case 8:
-                    hash[i] ^= (~inp[i%inp.length()]>>2);
-                    break;
-                case 9:
-                    hash[i] ^= (~inp[i%inp.length()]>>3);
-                    break;
-                case 10:
-                    hash[i] ^= (~inp[i%inp.length()]<<2);
-                    break;
-                case 11:
-                    hash[i] ^= (~inp[i%inp.length()]<<3);
-                    break;
-                case 12:
-                    hash[i] ^= (~inp[i%inp.length()]>>1<<1);
-                    break;
-                case 13:
-                    hash[i] ^= (~inp[i%inp.length()]>>2<<2);
-                    break;
-                case 14:
-                    hash[i] ^= (~inp[i%inp.length()]>>3<<3);
-                    break;
-     
+            case 0:
+                hash[i] ^= inp[a];
+                break;
+            case 1:
+                hash[i] ^= (inp[a] >> 2);
+                break;
+            case 2:
+                hash[i] ^= (inp[a] >> 1);
+                break;
+            case 3:
+                hash[i] ^= (inp[a] << 2);
+                break;
+            case 4:
+                hash[i] ^= (inp[a] << 1);
+                break;
+            case 5:
+                hash[i] ^= (~inp[a]);
+                break;
+            case 6:
+                hash[i] ^= (~inp[a] << 1);
+                break;
+            case 7:
+                hash[i] ^= (~inp[a] >> 1);
+                break;
+            case 8:
+                hash[i] ^= (~inp[a] >> 2);
+                break;
+            case 9:
+                hash[i] ^= (~inp[a] >> 3);
+                break;
+            case 10:
+                hash[i] ^= (~inp[a] << 2);
+                break;
+            case 11:
+                hash[i] ^= (~inp[a] << 3);
+                break;
+            case 12:
+                hash[i] ^= (~inp[a] >> 1 << 1);
+                break;
+            case 13:
+                hash[i] ^= (~inp[a] >> 2 << 2);
+                break;
+            case 14:
+                hash[i] ^= (~inp[a] >> 3 << 3);
+                break;
             }
         }
     }
 
-    cout << "Hash: \n";
-    for(int i =0;i<64;i++)
+    string out = "";
+    if (isBinary)
     {
-        cout << hex<< ((int)hash[i]/16+8);
-    }
-    cout << endl<<endl;
+        for (int i = 0; i < 64; i++) // convert to hex
+        {
+            int t = ((int)hash[i]) / 16 + 8;
+            cout << (t&1) << endl;
+            if((t&8)>0)
+            {
+                out += '1';
+            }
+            else
+            {
+                out += '0';
+            }
+            if((t&4)>0)
+            {
+                out += '1';
+            }
+            else
+            {
+                out += '0';
+            }
+            if((t&2)>0)
+            {
+                out += '1';
+            }
+            else
+            {
+                out += '0';
+            }
+            if((t&1)>0)
+            {
+                out += '1';
+            }
+            else
+            {
+                out += '0';
+            }
+        }
 
-    system("pause");
-    return 0;
+    }
+    else
+    {
+
+        for (int i = 0; i < 64; i++) // convert to hex
+        {
+            int t = ((int)hash[i]) / 16 + 8;
+
+            if (t < 10)
+                out += (char)(t + 48);
+            else
+            {
+                switch (t)
+                {
+                case 10:
+                    out += 'a';
+                    break;
+                case 11:
+                    out += 'b';
+                    break;
+                case 12:
+                    out += 'c';
+                    break;
+                case 13:
+                    out += 'd';
+                    break;
+                case 14:
+                    out += 'e';
+                    break;
+                case 15:
+                    out += 'f';
+                    break;
+                }
+            }
+        }
+    }
+    return out;
 }
